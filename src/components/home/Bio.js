1494b9730b095614a5d2e028024cc47c11be0bbd
Santhosh <sandyorbit@gmail.com>
2018-12-11 23:26:13 -0800

    bio update

--- src/components/home/Bio.js
+++ src/components/home/Bio.js
@@ -34,7 +34,7 @@ let bio = () => {
           models that exert least perceptual friction. <br />
           Have spent most of my recent years building data-driven products
           tailor-made for requirements ranging from self-containing exploratory
-          analysis tools to infographics for visual storytelling.<br />
+          analysis tools to interactive infographics for visual storytelling.<br />
         </p>
 
         <StyleRoot>
@@ -44,11 +44,11 @@ let bio = () => {
               marginBottom: -5,
             },
           }}>
-            I received a masters degree in Data Science from{' '}
+            I received my masters degree in Data Science from{' '}
             <a href="https://www.sice.indiana.edu/graduate/degrees/data-science/courses/index.html">
               Indiana University <FaGraduationCap />
-            </a>, Bloomington where I was introduced to info-vis and <a href='https://d3js.org/'>d3</a>{', '}
-            with which I ended up spending most of my time fascinated by visual encodings. The following is a wheel calandar inspired
+            </a>, Bloomington where I was introduced to info-vis and <a href='https://d3js.org/'>d3</a>{'. '}
+            I then ended up spending most of my time fascinated by visual encodings. The following is a wheel calandar inspired
             chart of time spent on various domains within data science until I started working full-time on data visualizations.
         </p>
         </StyleRoot>
