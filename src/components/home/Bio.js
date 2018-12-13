8b0bd1c654a365feb9a3693084f0b8da50c3975b
Santhosh <sandyorbit@gmail.com>
2018-12-13 00:54:21 -0800

    updated chess link and bio

--- src/components/home/Bio.js
+++ src/components/home/Bio.js
@@ -48,9 +48,9 @@ let bio = () => {
             I received my masters degree in Data Science from{' '}
             <a href="https://www.sice.indiana.edu/graduate/degrees/data-science/courses/index.html">
               Indiana University <FaGraduationCap />
-            </a>, Bloomington where I was introduced to info-vis and <a href='https://d3js.org/'>d3</a>{'. '}
-            I then ended up spending most of my time with it fascinated by visual encodings.
-            Would like to present this wheel calandar inspired
+            </a>, Bloomington where I was introduced to info-vis and <a href='https://d3js.org/'>d3</a>{', '}
+            with which I ended up spending most of my time, fascinated by visual encodings.
+            I would like to present this wheel calandar inspired
             chart of time spent on various domains within data science, starting from my masters upto work.
         </p>
         </StyleRoot>
